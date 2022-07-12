3986f7f4329ce3f24f0fcbbb5d570a614412347f
JoseSilva <odoo@conceptualdynamic.com>
2022-07-12 19:58:49 +0000

    actualizacion de avance

--- model_1/models/models.py
+++ model_1/models/models.py
@@ -9,9 +9,13 @@ class visit(models.Model):
     _description = 'visit'
 
     name = fields.Char(string='nombre')
-    last_name = fields.Char(string='apellido')
+    last_name = fields.Many2one(string='Cliente', comodel_name="res.partner")
     date = fields.Datetime(string='Fecha')
     type = fields.Selection([('0', 'Venezolano'),('1','Extrangero'),('2','Gobernamental')], string='Tipo', required=True)
     done = fields.Boolean(string='Realizada',readonly=True)
-
+    image = fields.Binary(string='Imagenes')
+    def toggle_state(self):
+        self.done = not self.done
     
+# class documentacion():
+#     _name = 'model_1.documentacion'
